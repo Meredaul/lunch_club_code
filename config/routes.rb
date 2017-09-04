ff28674fbb31a1410c2baf9df9cd701034f2b3f3
laury meredith <meredith.laury@gmail.com>
2017-09-05 00:34:43 +0100

    makes route for group creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+
   root to: 'pages#home'
+
+  resources :groups, only: [:new]
+
 end
