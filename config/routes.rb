fc39e4b13c382025c8ac384fb21320d3606642ae
laury meredith <meredith.laury@gmail.com>
2017-09-05 01:47:27 +0100

    linker icons basic

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'participations/new'
-
-  get 'groups/new'
-
-  get 'group/new'
-
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 
   root to: 'pages#home'
 
-  resources :groups, only: [:new]
+  get 'dashboard', to: 'pages#dashboard'
+
+  resources :groups, only: [:new, :index]
+
+  resources :participations, only: [:new]
 
 end
