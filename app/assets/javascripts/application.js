1bf8494127fb6d34a94e9ef507fe3877a01bcd31
laury meredith <meredith.laury@gmail.com>
2017-09-06 23:39:17 +0100

    not pushing the js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-//= require funs/index
-//= require funs/still_working_onit
+// require funs/index
+// require funs/still_working_onit
 
 //= require jquery
 //= require jquery_ujs
