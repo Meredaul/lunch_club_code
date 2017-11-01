4caa2ae2024b950b83ba4dd48d23a180d345bfce
laury meredith <meredith.laury@gmail.com>
2017-11-01 11:13:15 +0100

    finished login moadal

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  # match 'users/sign_out' => "devise/sessions#destroy"
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   root to: 'pages#home'
 
